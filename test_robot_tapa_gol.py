8be0ac4dcefa31976016378e2f7f84f54579384d
Kevin Paute <knpaute@espe.edu.ec>
2022-06-09 19:10:50 -0500

    Prueba unittest para agente

---
+++ test_robot_tapa_gol.py
@@ -0,0 +1,9 @@
+from hola import robot_tapa_gol
+import unittest
+
+class TestRobotTapaGol(unittest.TestCase):
+    def test_robot_tapa_gol(self):
+        self.assertEqual(robot_tapa_gol(), True)
+
+if __name__ == '__main__':
+    unittest.main()
